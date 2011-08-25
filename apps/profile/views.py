877a02a9be3fe2855b5fc4b7344cd21bd7da1d29
Yonsy Solis <ysolis@tribalperu.com>
2011-08-25 16:13:32 -0500

    profile views corrected

--- apps/profile/views.py
+++ apps/profile/views.py
@@ -2,10 +2,9 @@
 
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from forms import LoginForm, ProfileForm
 
-from main.views import not_implemented
+from models import Profile
 
 
 def profile_login(request):
@@ -16,22 +15,30 @@ def profile_login(request):
     if request.method == 'POST':
         login_form = LoginForm(request.POST)
         if login_form.is_valid():
-            user, created = User.get_or_create(username = request.POST["email"],password = request.POST["birth_date"])
-            if created:
+            try:
+                profile = Profile.objects.get(email=login_form.cleaned_data['email'])
+                return redirect('profile_enter_code')
+            except Profile.DoesNotExist:
                 return redirect('profile_register')
-            else:
-                return redirect('main_homepage')
-    login_form = LoginForm()
+    else:
+        login_form = LoginForm()
     return render(request, 'profile/profile_login.html', {
         'login_form': login_form, 
         })
 
+
 def profile_register(request):
+    """
+    Register new profile, if success, login and go to enter code page
+    else, error and try again
+    """
     if request.method == 'POST':
         profile_form = ProfileForm(request.POST)
         if profile_form.is_valid():
-            profile_form.save()
-            return not_implemented(request)
+            profile = profile_form.save()
+            login(request, profile.user)
+            return redirect('profile_enter_code')
+
     profile_form = ProfileForm(request.POST)
     return render('profile/profile_register.html', {
         'profile_form': profile_form, 
