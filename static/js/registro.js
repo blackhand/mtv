00f7b20ed8cbd059b35431a5b4f7d30c8b1d0d70
Yonsy Solis <ysolis@tribalperu.com>
2011-09-07 23:57:50 -0500

    hasta aqui llegue, blame jquery 1.4.x

---
+++ static/js/registro.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('a.btn-enviar').click( function(){
+
+        //$(".clients").click( function() { alert( "Here's some information." ) } );
+        //$(".clients").live( "click", function() { alert( "Here's some information." ) } );
+
+        $('.form2').removeClass('not-visible');
+        $('.form1').addClass('not-visible');
+        //$('#iframe_ga').attr('src','inicio.html');
+        //$('#iframe_ga').load();
+    });
+});
