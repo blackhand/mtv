5a17714fef5094efc14bb803f5ff1ea72e895744
Yonsy Solis <yonsy.solis@gmail.com>
2011-09-08 10:02:38 -0500

    registro.js funcion basica

--- static/js/registro.js
+++ static/js/registro.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
+    // great ... all forms use the same clases for send :/
     $('a.btn-enviar').click( function(){
 
-        //$(".clients").click( function() { alert( "Here's some information." ) } );
-        //$(".clients").live( "click", function() { alert( "Here's some information." ) } );
-
-        $('.form2').removeClass('not-visible');
         $('.form1').addClass('not-visible');
+        $('.form2').removeClass('not-visible');
+        $('.scroll-pane').jScrollPane();
         //$('#iframe_ga').attr('src','inicio.html');
         //$('#iframe_ga').load();
     });
 });
+
+function validate() {
+    return true;
+}
