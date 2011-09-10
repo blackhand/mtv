d65597bd996d027696c7cdc98b1603b62d6379e9
Yonsy Solis <ysolis@tribalperu.com>
2011-09-09 20:11:37 -0500

    delete things

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,9 +1,1 @@
 # -*- coding: utf-8 -*-
-
-from django import forms
-from ubigeo.forms import UbigeoField
-
-class LoginForm(forms.Form):
-    email = forms.EmailField()
-    ubigeo = UbigeoField()
-
