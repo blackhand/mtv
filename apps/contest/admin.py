d53f57440dadb56ab040826b3f0fbad280e90ada
Yonsy Solis <ysolis@tribalperu.com>
2011-09-09 20:53:16 -0500

    fix admin

--- apps/contest/admin.py
+++ apps/contest/admin.py
@@ -4,10 +4,8 @@ Admin for Profile model
 """
 
 from django.contrib import admin
-from models import Product, Option, Draw
+from models import Option, Draw
 
-
-admin.site.register(Product)
 admin.site.register(Option)
 admin.site.register(Draw)
 
