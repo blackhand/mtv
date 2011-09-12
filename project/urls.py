8733fd2fc9f05588bbf60726d419a9f872b8e3f6
Marco Ordonez <mordonez@perfectmedia.com.pe>
2011-09-12 02:33:53 -0500

    Urls para analytics y cambio en rutas con / en registro.js

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     # Profile URLs
@@ -20,6 +21,16 @@ urlpatterns = patterns('',
 
 	#Facebook
     url(r'^facebook_save/', 'facebook.views.save', name='facebook_save'),
+
+	#Directos GA
+	url(r'^login.html',direct_to_template,{'template':'ga/login.html'},name='login.html'),
+	url(r'^compartir.html',direct_to_template,{'template':'ga/compartir.html'},name='compartir.html'),
+	url(r'^premios.html',direct_to_template,{'template':'ga/premios.html'},name='premios.html'),
+	url(r'^empaques.html',direct_to_template,{'template':'ga/empaques.html'},name='empaques.html'),
+	url(r'^comercial.html',direct_to_template,{'template':'ga/comercial.html'},name='comercial.html'),
+
+
+
 
 )
 
